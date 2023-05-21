c60f860f03102fcb8061e761d5a52cb38406b545
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-21 12:32:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function volume_sphere() {
     //Write your code here
    let r=parseInt(document.getElementById("radius").value);
-	document.getElementById("volume").value=(4/3)*3.14*r*r*r;
+	document.getElementById("volume").value=((4/3)*3.14*r*r*r).toFixed(4);
 	
 } 
 
