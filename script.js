16273b92e458e323c8acb4f78d8941fa808b6c32
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-21 12:14:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+   let r=parseInt(document.getElementById("radius").value);
+	document.getElementById("volume").value=(4/3)*3.14*r*r*r;
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
